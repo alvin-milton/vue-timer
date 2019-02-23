3fba35cbf2f90798b1ed164fa8d8925e5e8f8f10
Alvin Milton <al@thehandiest.com>
2019-02-22 23:29:46 -0500

    a vue app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Reactivity</h1>
+    <p>{{ seconds }} have elapsed since this page is open.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      seconds: 0
+    }
+  },
+  // create runs when the app is initiated
+  created () {
+    setInterval( () => {
+      this.seconds++;
+    }, 1000);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
